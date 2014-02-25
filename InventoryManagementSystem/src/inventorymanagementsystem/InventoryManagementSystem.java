03482ebc15524431aabd8e4587ff03eaa9ec75ae
SnoopSqueak <SnoopSqueak@aim.com>
2014-02-25 15:39:09 -0600

    Started Coding
    I used NetBeans' Java GUI editor to make a functionless rough draft of
    our application. I also included a screenshot.

---
+++ InventoryManagementSystem/src/inventorymanagementsystem/InventoryManagementSystem.java
@@ -0,0 +1,18 @@
+
+package inventorymanagementsystem;
+
+/**
+ *
+ * @author Mike
+ */
+public class InventoryManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        IMSGUI imsGui = new IMSGUI();
+        imsGui.setVisible(true);
+    }
+}
