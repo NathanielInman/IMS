472d52bdd7f47c895656d47092c1df27205e50a0
Nathaniel B. Inman <nate@theoestudio.com>
2014-03-10 14:25:11 -0500

    Revised JDBC to Mysql migration
    Revised JDBC to Mysql migration. Currently synchonizing the remote
    server to establish a secure and easily maintained access channel.

--- InventoryManagementSystem/src/inventorymanagementsystem/InventoryManagementSystem.java
+++ InventoryManagementSystem/src/inventorymanagementsystem/InventoryManagementSystem.java
@@ -10,7 +10,11 @@ public class InventoryManagementSystem {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        /*testing database
+        DatabaseController databaseController = new DatabaseController();
+        databaseController.readDatabase();
+        */
         InventoryController inventoryController = new InventoryController();
         IMSGUI imsGui = new IMSGUI();
         imsGui.setController(inventoryController);
