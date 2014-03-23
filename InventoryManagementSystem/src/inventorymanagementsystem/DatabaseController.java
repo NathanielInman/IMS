95409fd9b08a3db9e36a632651f7be2fc45106bc
Nathaniel B. Inman <nate@theoestudio.com>
2014-03-23 17:52:51 -0500

    IMS - Database Controller Remote Connection
    Made some changes, adjusting SSH - it's now tunneling fine to the
    server, but 1and1 re-routes their ssh through to another one of their
    servers, so now i'm working with them on setting up /etc/my.cnf to allow
    remote connections, etc.

--- InventoryManagementSystem/src/inventorymanagementsystem/DatabaseController.java
+++ InventoryManagementSystem/src/inventorymanagementsystem/DatabaseController.java
@@ -10,27 +10,62 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+import java.sql.SQLException;
+
 public class DatabaseController {
-    private Connection connect = null;
-    private Statement statement = null;
-    private PreparedStatement preparedStatement = null;
-    private ResultSet resultSet = null;
-    public void readDatabase() throws Exception{
+    static int lport;
+    static String rhost;
+    static int rport;
+    public static void go(){
+        String user = "u68221731";
+        String password = "P#n3l0p@";
+        String host = "plainsofsedia.com";
+        int port=22;
         try{
-            Class.forName("com.mysql.jdbc.Driver"); //load mysql driver
-            connect = DriverManager.getConnection(
-                    "jdbc:mysql://db517952926.db.1and1.com/feedback?" +
-                    "user=dbo517952926&password=KerleyCopyCenter1!");
-            statement = connect.createStatement();
-            resultSet = statement.executeQuery("select * from Inventory");
-        }catch(Exception e){
-            throw e;
-        }finally{
-            try{
-                if(resultSet!=null)resultSet.close();
-                if(statement!=null)statement.close();
-                if(connect!=null)connect.close();
-            }catch(Exception e){throw e;}
+            JSch jsch = new JSch();
+            Session session = jsch.getSession(user, host, port);
+            lport = 4321;
+            rhost = "localhost";
+            rport = 3306;
+            session.setPassword(password);
+            session.setConfig("StrictHostKeyChecking", "no");
+            System.out.println("Establishing Connection...");
+            session.connect();
+            int assinged_port=session.setPortForwardingL(lport, rhost, rport);
+            System.out.println("localhost:"+assinged_port+" -> "+rhost+":"+rport);
+        }catch(Exception e){System.err.print(e);}
+    } //end go()
+    public static void main(String[] args) {
+        try{
+            go();
+        }catch(Exception ex){}
+        System.out.println("An example for updating a Row from Mysql Database!");
+        Connection con = null;
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://" + rhost +":" + lport + "/";
+        String db = "db517952926.db.1and1.com";
+        String dbUser = "dbo517952926";
+        String dbPasswd = "KerleyCopyCenter1!";
+        try{
+            Class.forName(driver);
+            con = DriverManager.getConnection(url+db, dbUser, dbPasswd);
+            /*try{
+                Statement st = con.createStatement();
+                String sql = "UPDATE MyTableName " +
+                    "SET email = 'ripon.wasim@smile.com' WHERE email='peace@happy.com'";
+                int update = st.executeUpdate(sql);
+                if(update >= 1){
+                    System.out.println("Row is updated.");
+                }else{
+                    System.out.println("Row is not updated.");
+                } //end if
+            }catch (SQLException s){
+                System.out.println("SQL statement is not executed!");
+            } //end try */
+        }catch (Exception e){
+            e.printStackTrace();
         } //end try
-    } //end readDatabase()
-} //end DatabaseController()
+    } //end main
+} //end class
