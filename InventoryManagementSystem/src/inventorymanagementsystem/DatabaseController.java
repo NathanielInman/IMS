57883ca1403271bd3e04a9704578331e892ec87c
Nathaniel B. Inman <nate@theoestudio.com>
2014-03-23 18:32:45 -0500

    Changed from Mysql to Derby local SQL connection
    Changed from Mysql to Derby local SQL connection since host wasn't
    willing to allow connection to /etc/my.cnf to allow remote connections.
    Currently pulls dummy data from Inventory.

--- InventoryManagementSystem/src/inventorymanagementsystem/DatabaseController.java
+++ InventoryManagementSystem/src/inventorymanagementsystem/DatabaseController.java
@@ -4,68 +4,44 @@
  */
 package inventorymanagementsystem;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.Session;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DatabaseController {
-    static int lport;
-    static String rhost;
-    static int rport;
-    public static void go(){
-        String user = "u68221731";
-        String password = "P#n3l0p@";
-        String host = "plainsofsedia.com";
-        int port=22;
-        try{
-            JSch jsch = new JSch();
-            Session session = jsch.getSession(user, host, port);
-            lport = 4321;
-            rhost = "localhost";
-            rport = 3306;
-            session.setPassword(password);
-            session.setConfig("StrictHostKeyChecking", "no");
-            System.out.println("Establishing Connection...");
-            session.connect();
-            int assinged_port=session.setPortForwardingL(lport, rhost, rport);
-            System.out.println("localhost:"+assinged_port+" -> "+rhost+":"+rport);
-        }catch(Exception e){System.err.print(e);}
-    } //end go()
-    public static void main(String[] args) {
-        try{
-            go();
-        }catch(Exception ex){}
-        System.out.println("An example for updating a Row from Mysql Database!");
-        Connection con = null;
-        String driver = "com.mysql.jdbc.Driver";
-        String url = "jdbc:mysql://" + rhost +":" + lport + "/";
-        String db = "db517952926.db.1and1.com";
-        String dbUser = "dbo517952926";
-        String dbPasswd = "KerleyCopyCenter1!";
-        try{
-            Class.forName(driver);
-            con = DriverManager.getConnection(url+db, dbUser, dbPasswd);
-            /*try{
-                Statement st = con.createStatement();
-                String sql = "UPDATE MyTableName " +
-                    "SET email = 'ripon.wasim@smile.com' WHERE email='peace@happy.com'";
-                int update = st.executeUpdate(sql);
-                if(update >= 1){
-                    System.out.println("Row is updated.");
-                }else{
-                    System.out.println("Row is not updated.");
-                } //end if
-            }catch (SQLException s){
-                System.out.println("SQL statement is not executed!");
-            } //end try */
-        }catch (Exception e){
-            e.printStackTrace();
-        } //end try
+   static final String JDBC_DRIVER = "org.apache.derby.jdbc.ClientDriver";
+   static final String DB_URL = "jdbc:derby://localhost:1527/KCC"; //jdbc:derby://localhost:1527/KCC [KerleyCopyCenter on KERLEYCOPYCENTER]
+   static final String USER = "KerleyCopyCenter";
+   static final String PASS = "KerleyCopyCenter";
+   public static void main(String[] args){
+       Connection conn = null;
+       Statement stmt = null;
+       try{
+           Class.forName(JDBC_DRIVER);
+           System.out.println("Connecting to database...");
+           conn = DriverManager.getConnection(DB_URL,USER,PASS);
+           System.out.println("Creating statement...");
+           stmt = conn.createStatement();
+           String sql;
+           sql = "SELECT * FROM Inventory";
+           ResultSet rs = stmt.executeQuery(sql);
+           while(rs.next()){
+               int id= rs.getInt("NUMBER");
+               String name=rs.getString("NAME");
+               System.out.println(id+":"+name);
+           } //end while
+           rs.close();
+           stmt.close();
+           conn.close();
+       }catch(SQLException se){ //errors in the SQL processing
+           se.printStackTrace();
+       }catch(Exception e){ //errors relating to Class.forName
+           e.printStackTrace();
+       }finally{ //clean up regardless of success
+            try{
+                if(stmt!=null)stmt.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            } //end try
+       } //end try
+       System.out.println("Finished.");
     } //end main
 } //end class
