1bd34b7ef45a57cd7d75dea27e09ba74a0c03edc
Nathaniel B. Inman <nate@theoestudio.com>
2014-04-13 12:42:55 -0500

    Interface Update has begun.
    Will be creating a custom Look and Feel based upon JTattoo. The window
    is also now centered. Removed main() from the gui since it's no longer
    being called

--- InventoryManagementSystem/src/inventorymanagementsystem/IMSGUI.java
+++ InventoryManagementSystem/src/inventorymanagementsystem/IMSGUI.java
@@ -1,11 +1,12 @@
 package inventorymanagementsystem;
 
 import javax.swing.BoxLayout;
+import javax.swing.UIManager;
 
 
 /**
  *
- * @author Mike
+ * @author Mike & Nate
  * Most of this was done with NetBeans' Java GUI editor.
  * Icons used are from https://www.iconfinder.com/search/?q=iconset:linecons-free-vector-icons-pack
  * We'll need to include credit as specified by the Creative Commons license on that page if we use
@@ -283,46 +284,6 @@ public class IMSGUI extends javax.swing.JFrame {
         gearMenu.add(gearMenuPH1);
     }//GEN-LAST:event_jLabel2MouseClicked
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /*
-         * Set the Nimbus look and feel
-         */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /*
-         * If Nimbus (introduced in Java SE 6) is not available, stay with the
-         * default look and feel. For details see
-         * http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(IMSGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(IMSGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(IMSGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(IMSGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /*
-         * Create and display the form
-         */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new IMSGUI().setVisible(true);
-            }
-        });
-    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel controllerDisplay;
     private javax.swing.JPopupMenu gearMenu;
--- InventoryManagementSystem/src/inventorymanagementsystem/InventoryManagementSystem.java
+++ InventoryManagementSystem/src/inventorymanagementsystem/InventoryManagementSystem.java
@@ -1,6 +1,8 @@
 
 package inventorymanagementsystem;
 
+import javax.swing.UIManager;
+
 /**
  *
  * @author Mike
@@ -11,13 +13,21 @@ public class InventoryManagementSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
-        /*testing database
-        DatabaseController databaseController = new DatabaseController();
-        databaseController.readDatabase();
-        */
+        try {
+            UIManager.setLookAndFeel("com.jtattoo.plaf.hifi.HiFiLookAndFeel");
+         } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(IMSGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(IMSGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(IMSGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(IMSGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
         InventoryController inventoryController = new InventoryController();
         IMSGUI imsGui = new IMSGUI();
         imsGui.setController(inventoryController);
+        imsGui.setLocationRelativeTo(null);
         imsGui.setVisible(true);
     }
 }
